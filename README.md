390443d806fc1c6e603cfeaef292ea033e512f5e
Daniel Serpell <daniel.serpell@gmail.com>
2025-12-26 20:30:40 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Label Web Creator
+
+Print labels through Phomemo D30 or Q30 printers.
+
---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Web Label Printer</h1>
+
+</main>
+
+<style>
+</style>
